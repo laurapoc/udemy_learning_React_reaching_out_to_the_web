61e9711de6dce60bbcc7201ef1a60edec5819384
laurapoc <laura.pociuniene@gmail.com>
2020-10-20 13:31:33 +0300

    extract Route parameters and use Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 import Posts from "../Blog/Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
-
-
-
   render() {
     return (
       <div className="Blog">
@@ -16,24 +14,32 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink to="/" 
-                exact
-                activeClassName="my-active"
-                activeStyle={{
-                  color: "#fa923f",
-                  textDecoration: "underline"
-                }}>Home</NavLink>
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
                 {/* This is the way not to reload the page every time when clicking on the inner NavLink.
                 In this way react rerenders only those parts that needed to be reloaded. 
                 We are not loading the same page again: */}
-                <NavLink to={{
-                  // Absolute path:
-                  pathname: "/new-post",
-                  hash: "#submit",
-                  search: "?quick-submit=true"
-                }}>New Post</NavLink>
+                <NavLink
+                  to={{
+                    // Absolute path:
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
@@ -41,7 +47,10 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
