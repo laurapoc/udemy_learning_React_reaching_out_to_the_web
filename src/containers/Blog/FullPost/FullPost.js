3db918383275b35d9e76e7e7562d6c60d304eec0
laurapoc <laura.pociuniene@gmail.com>
2020-10-20 18:21:38 +0300

    handle changes in componentDidUpdate to load diferent posts

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,8 +10,16 @@ class FullPost extends Component {
 
   componentDidMount() {
     console.log(this.props.match.params.id);
+    this.loadData();
+  }
+
+  componentDidUpdate() {
+    this.loadData();
+  }
+
+  loadData() {
     if (this.props.match.params.id) {
-      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
         axios.get("/posts/" + this.props.match.params.id).then((response) => {
           // console.log(response);
           this.setState({ loadedPost: response.data });
@@ -21,15 +29,14 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id)
-    .then(response => {
-        console.log(response);
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
+      console.log(response);
     });
-  }
+  };
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: "center" }}>Loading...!</p>;
     }
     if (this.state.loadedPost) {
@@ -38,7 +45,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
           </div>
         </div>
       );
